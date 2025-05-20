c9991d259329ce85a295b78ee0279363205de37e
vietanh28 <hoangvietanh730@gmail.com>
2025-05-20 15:21:09 +0100

    Add user register, JWT auth

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from . import views
+
+urlpatterns = [
+    path('register/', views.UserRegistrationView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
