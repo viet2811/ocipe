10eb20899c7754a32ed21acaeeda8fc47a0677e0
vietanh28 <hoangvietanh730@gmail.com>
2025-05-22 22:19:37 +0100

    Fix searching by multi ingredients
    A bug was cause when making request and adding 1 single space between comma. Fixed by using strip() and worked perfectly yippie

--- recipes/filters.py
+++ recipes/filters.py
@@ -6,18 +6,18 @@ class RecipeFilter(django_filters.FilterSet):
     ingredients = django_filters.CharFilter(method='filter_by_ingredients')
 
     def filter_by_ingredients(self, queryset, name, value):
-        ingredient_names = value.split(',')
-        queryset = (queryset
-                    .filter(ingredients__name__in=ingredient_names)
-                    .annotate(
-                        matched_count=Count(
-                            "ingredients",
-                            filter=Q(ingredients__name__in=ingredient_names),
-                            distinct=True
-                        )   
-                    )
-                    .order_by("-matched_count")
-        )
+        # Prevent bug by leaving an empty space before comma
+        ingredient_names = [name.strip() for name in value.split(',') if name.strip()]
+        # Return recipe rank on the most ingredient match with the request, excluding ones with no matched
+        queryset = queryset.annotate(
+            matched_count=Count(
+                "ingredients",
+                filter=Q(ingredients__name__in=ingredient_names),
+                distinct=True
+            )
+        ).filter( 
+            matched_count__gt=0   
+        ).order_by('-matched_count')
         return queryset
     
     class Meta:
