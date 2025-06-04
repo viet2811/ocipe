5ecc9f82d74d0767168e13a0c65d10c9b9c74034
vietanh28 <hoangvietanh730@gmail.com>
2025-06-04 18:29:02 +0100

    Adjust meat_type max length in db

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Recipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    meat_type = models.CharField(max_length=15)
+    meat_type = models.CharField(max_length=30)
     longevity = models.IntegerField()
     frequency = models.CharField(max_length=20)
     note = models.TextField(blank=True)
