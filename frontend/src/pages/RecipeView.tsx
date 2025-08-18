ebec1a6ded008f06c830ad3d9de3199893eba277
vietanh28 <49806901+viet2811@users.noreply.github.com>
2025-08-18 22:04:24 +0700

    Add ability to export recipes data
    Also add an Import Input Dialog, which is TODO: validate file, correct type, and bulk create on send POST each recipe with for loops

--- frontend/src/pages/RecipeView.tsx
+++ frontend/src/pages/RecipeView.tsx
@@ -16,6 +16,48 @@ import { Import, RefreshCw, Share, Trash } from "lucide-react";
 import { toast } from "sonner";
 import { queryClient } from "@/lib/queryClient";
 import RecipeList from "../components/RecipeList";
+import { type Recipe, type RecipeInput } from "@/types/recipes";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { useRef, useState } from "react";
+
+function toRecipeInputs(recipes: Recipe[]): RecipeInput[] {
+  return recipes.map((r) => ({
+    name: r.name,
+    meat_type: r.meat_type,
+    longevity: r.longevity,
+    frequency: r.frequency,
+    note: r.note, // optional
+    state: r.state,
+    ingredients: r.ingredient_list.map((ing) => ({
+      name: ing.name,
+      quantity: ing.quantity,
+    })),
+  }));
+}
+
+function downloadRecipesAsJson(recipes: RecipeInput[]) {
+  const dataStr = JSON.stringify(recipes, null, 2); // pretty print
+  const blob = new Blob([dataStr], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = "recipes.json"; // filename
+  link.click();
+
+  URL.revokeObjectURL(url); // cleanup
+}
 
 export default function RecipeView() {
   const deleteAllMutation = useMutation({
@@ -38,15 +80,56 @@ export default function RecipeView() {
       toast.error("Something went wrong. Please retry");
     },
   });
+  const closeDialogRef = useRef<HTMLButtonElement>(null);
 
   const LeftSideButtons: React.FC = () => (
     <div className="grid grid-cols-2 gap-2 mb-2 @md:flex">
-      <Button size="sm">
+      {/* Export */}
+      <Button
+        size="sm"
+        onClick={() => {
+          let recipe_data = queryClient.getQueryData<Recipe[]>(["recipes"]);
+          if (recipe_data) {
+            let converted = toRecipeInputs(recipe_data);
+            downloadRecipesAsJson(converted);
+          }
+        }}
+      >
         <Share /> Export
       </Button>
-      <Button size="sm" variant="outline">
-        <Import /> Import
-      </Button>
+
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button size="sm" variant="outline">
+            <Import /> Import
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Import recipes</DialogTitle>
+            <DialogDescription>
+              Add the JSON file which was exported from Ocipe
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-3">
+            <Label htmlFor="file">Upload JSON</Label>
+            <Input
+              id="file"
+              type="file"
+              accept=".json"
+              onClick={(e) => e.stopPropagation()}
+            />
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline" ref={closeDialogRef}>
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button type="button">Submit</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
       {/* Reset status */}
       <Button
         variant="secondary"
