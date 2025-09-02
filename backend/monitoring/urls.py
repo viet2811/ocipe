4921cee78b5a4866ab2be454a8fda417964796c2
vietanh28 <49806901+viet2811@users.noreply.github.com>
2025-09-02 14:25:10 +0700

    Update docker file for deployment

--- backend/monitoring/urls.py
+++ backend/monitoring/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.health, name="check-health"),
+    path("healthz/", views.health, name="check-health"),
     path("db/", views.db_ping, name="db_ping"),
 ]
